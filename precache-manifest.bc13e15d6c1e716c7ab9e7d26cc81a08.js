ba62f3e0acff0dc06f93dcaec38c3cb9e375370d
LJS22 <LJS22@users.noreply.github.com>
2020-07-28 10:27:51 +0100

    Updates

---
+++ precache-manifest.bc13e15d6c1e716c7ab9e7d26cc81a08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d7b4b56d25bcffb93604f5dd52b42b",
+    "url": "/To-Do-List-App/index.html"
+  },
+  {
+    "revision": "52b6a3bf4f816070ef3d",
+    "url": "/To-Do-List-App/static/css/main.83cbd2df.chunk.css"
+  },
+  {
+    "revision": "65cb2e9f31e48650928f",
+    "url": "/To-Do-List-App/static/js/2.f158a218.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-List-App/static/js/2.f158a218.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52b6a3bf4f816070ef3d",
+    "url": "/To-Do-List-App/static/js/main.f281c735.chunk.js"
+  },
+  {
+    "revision": "2b20a5c97a2db90e4cb9",
+    "url": "/To-Do-List-App/static/js/runtime-main.f789cd44.js"
+  }
+]);
